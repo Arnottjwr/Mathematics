d1524b539c01a07c9170c26310942455ffb93ebf
Jack Arnott <140889988+Arnottjwr@users.noreply.github.com>
2024-04-01 19:44:52 +0100

    Update Bifurcation Plot_bio.py

--- Mathematical Biology/Bifurcation Plot_bio.py
+++ Mathematical Biology/Bifurcation Plot_bio.py
@@ -27,7 +27,7 @@ for i in range(iterations):
     if i >= (iterations - last):
         ax1.plot(r, x, ',b', alpha=.25)
 # Plot
-ax1.set_xlim(3.5,4)
+ax1.set_xlim(0,4)
 ax1.set_ylim(0,1)
 ax1.set_title("Bifurcation diagram of $f(N) = rN(1 - N)$")
 ax1.set_xlabel('r')
