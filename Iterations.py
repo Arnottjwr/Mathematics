bcc9445db9a580b23d0a2ccd9c834e9d13c658f1
Jack Arnott <140889988+Arnottjwr@users.noreply.github.com>
2024-04-01 19:36:11 +0100

    Update Iterations.py

--- Iterations.py
+++ Iterations.py
@@ -1,26 +1,33 @@
+'''
+Code to test iterations of the logistic difference equation for differing values of control parameter r 
+The logistic map displays rich dynamical behaviour. As the control parameter r (0 < r < 4) varies, 
+we observe period doubling bifurcations. See the Bifurcation diagram plot for code to plot a Bifurcation diagram 
+'''
+
+
 import numpy as np 
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 10})
 
-# Function 
+### Logistic Map 
 def logistic(x, r):
     return r*x*(1-x)
 
-# Inital Data 
-r = 0.5
-initial = 0.5
-n = 75
+### Inital Data 
+r = 0.5 # Control Parameter 
+initial = 0.5 # Inital Value 
+n = 75 # Iterations 
 
-# Arrays 
+### Arrays 
 x = np.linspace(0,n,n)
 y = np.zeros_like(x)
 y[0] = logistic(initial, r)
 
-# Iterate 
+### Iterate 
 for i in range(1,n):
     y[i] = logistic(y[i-1],r)
 
-# Plot   
+### Plot   
 fig,ax = plt.subplots(1,figsize = (10,6))
 ax.set_ylim(0,1)
 ax.set_title(f'{n} iterations of $f(N) = rN(1-N) $ with r = {r} and inital point = {initial}')
