acfb3ef2318ea3aaf92e1dbfc4748fb0792f38b7
Jack Arnott <140889988+Arnottjwr@users.noreply.github.com>
2024-04-03 09:22:21 +0100

    Add files via upload

---
+++ Mathematical Biology/Tangent_bifurcation.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+plt.rcParams.update({'font.size': 10})
+
+# Plot to illustrate Tangent Bifurcation 
+
+# Logistic Map
+def logistic(x,r):
+    return r*x*(1 - x)
+
+# Parameters 
+r = 1 + np.sqrt(8)
+x = np.linspace(0,1,500)
+f3 = logistic(logistic(logistic(x,r),r),r)
+
+# Plot   
+fig,ax = plt.subplots(1,figsize = (10,6))
+ax.set_ylim(0,1)
+ax.set_title(f'$f^3(N_n)$ against $x$ for r = {r}')
+ax.set_xlabel('$N_n')
+ax.set_ylabel('$f^3(N_n)$')
+ax.plot(x,f3)
+ax.plot(x,x)
+ax.grid()
+plt.show()
