14c6cd6b8acbad58ef0e74ce1a021ee82792771a
Jack Arnott <jack.arnott002@gmail.com>
2024-11-30 14:53:14 +0000

    Data generator

---
+++ Dissertation/wave_data_generator.py
@@ -0,0 +1,14 @@
+import  numpy               as np
+import  matplotlib.pyplot   as plt
+from    WaveFunctions       import plucked_string
+
+def generate_data(space_array: np.ndarray, time_array: np.ndarray, noise: bool) -> np.ndarray:
+    """
+    Generate a dataset from the analytic solution to the wave equation
+
+    :param space_array: Spatial Domain
+    :param time_array: Temporal Domain
+    :param noise: Noise Toggle 
+    :return: Data Set
+    """
+    pass
